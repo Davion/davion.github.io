4205b0a5489f422da8469100e1acb49c7bc4764d
Klajdi Hoxha <klainti95@gmail.com>
2021-06-16 00:45:57 +0200

    dark theme finish

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
-const darkModeToggle = document.querySelector("#switch");
-const sunIcon = document.querySelector("#sunIcon");
-const moonIcon = document.querySelector("#moonIcon");
+let darkmode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
+
+const enableDarkMode = () => {
+    document.body.classList.add("darkmode");
+
+    localStorage.setItem("darkMode", "enabled");
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove("darkmode");
+
+    localStorage.setItem("darkMode", null);
+}
+
+const trans = () => {
+    document.documentElement.classList.add("transition");
+    window.setTimeout(() => {
+        document.documentElement.classList.remove("transition");
+    }, 1000)
+}
+
+if(darkmode === "enabled"){
+    darkModeToggle.checked = true;
+    enableDarkMode();
+}
 
 darkModeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("darkmode");
+    darkmode = localStorage.getItem("darkMode");
+    if(darkmode !== "enabled"){
+        trans();
+        enableDarkMode();
+    }else{
+        trans();
+        disableDarkMode();
+    }
 });
